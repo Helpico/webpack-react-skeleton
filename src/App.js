2b383478b297acc3d5f57659fffaf4d4f3c60f3e
Maksym Dudyk <novamova@ukr.net>
2022-01-22 23:26:48 +0200

    Counter in redux/toolkit completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
-import image from './images/floating.svg'
 import './styles/index.css'
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, increaseBy20 } from "./redux/counterSlice";
+
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
-  const couple = 'John & Marry'
-
-    return (
+  return (
       <div>
-        <h1>Hello, {couple}</h1>
-        <div>
-          <img width="70%" src={image} />
-        </div>
+        <button onClick={() => dispatch(decrement())}>decrement</button>
+        <comment>{count}</comment>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button onClick={() => dispatch(increaseBy20(20))}>+ 20</button>
+        
       </div>
     )
 }
