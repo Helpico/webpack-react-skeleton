651fa9039784c6d401b3703685352cb3fdda2a99
Maksym Dudyk <novamova@ukr.net>
2022-02-16 21:55:55 +0200

    Starter My Create React App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from "react";
-import './styles/index.css'
-import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement, increaseBy20 } from "./redux/counterSlice";
-
 
 function App() {
-  const count = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
-
   return (
       <div>
-        <button onClick={() => dispatch(decrement())}>decrement</button>
-        <comment>{count}</comment>
-        <button onClick={() => dispatch(increment())}>increment</button>
-        <button onClick={() => dispatch(increaseBy20(20))}>+ 20</button>
-        
+        React Webpack Handmade Boilerplate
       </div>
     )
 }
